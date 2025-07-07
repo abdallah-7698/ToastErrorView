09021404e07dd12a87f85e2ce88a9f8cd3920b0a
abdallah.mohammedabdallah.mohammedabdallah.mohammed <abdallah.moha.github.comabdallah-moha@gmail.com>
2025-07-07 03:41:34 +0300

    Initial Commit

---
+++ ErrorView/ErrorViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorViewApp.swift
+//  ErrorView
+//
+//  Created by name on 07/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ErrorViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
